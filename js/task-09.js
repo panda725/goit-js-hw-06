3e48545051964461e2054d6a46f7e91341f0651a
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-09 04:11:10 -0400

    ended with task #9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  body: document.querySelector('body'),
+  spanBackgroundColor: document.querySelector('.color'),
+  btnChangeColor: document.querySelector('.change-color'),
+};
+
+refs.btnChangeColor.addEventListener('click', event => {
+  let backGroundColor = getRandomHexColor();
+  refs.spanBackgroundColor.textContent = backGroundColor;
+  refs.body.style.backgroundColor = `${backGroundColor}`;
+  console.log(backGroundColor);
+});
