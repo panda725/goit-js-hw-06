c402f6e690161df5106f35fbab5e125cfdaebb4b
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-09 13:28:36 -0400

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,15 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
 
-const categorEl = document.querySelector("#categories");
-console.log("Number of categories:", categorEl.querySelectorAll(".item").length);
+const itemsAll = document.querySelectorAll('.item');
+console.log('Number of categories:', itemsAll.length);
 
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
 
-const nameOfCategor = (a) => {
-  const itemsAll = document.querySelectorAll(".item");
-  itemsAll.forEach((item) => {
-    console.log("Category:", item.querySelector("h2").textContent);
-    console.log("Elements:", item.querySelectorAll("li").length);
+const nameOfCategor = a => {
+  itemsAll.forEach(item => {
+    console.log('Category:', item.querySelector('h2').textContent);
+    console.log('Elements:', item.querySelectorAll('li').length);
   });
 };
 
-nameOfCategor(categorEl);
+nameOfCategor(itemsAll);
