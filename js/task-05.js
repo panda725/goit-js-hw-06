25323c13844c00880000db1b6a1103ebf9a9f2a1
Alex Supotnytskyi <kedred725@gmail.com>
2022-08-09 13:35:35 -0400

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,8 @@ const output = document.querySelector('#name-output');
 textInput.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-  output.textContent = event.currentTarget.value;
-  if (event.currentTarget.value === '') {
+  output.textContent = event.currentTarget.value.trim();
+  if (!event.currentTarget.value) {
     output.textContent = 'Anonymous';
   }
 }
